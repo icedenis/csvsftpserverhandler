3fdd905ef11385fee09cf4437dcf0df8ab73007c
icedenis <icedenis100@gmail.com>
2022-02-18 18:22:57 +0100

    first commit

---
+++ src/main/java/com/example/csvsftpserverhandler/CsvsftpserverhandlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.csvsftpserverhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvsftpserverhandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvsftpserverhandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/csvsftpserverhandler/CsvsftpserverhandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csvsftpserverhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvsftpserverhandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
